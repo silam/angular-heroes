31c10ea44e24366270a63e036c932770c22b14b9
Si Lam <silam@hotmail.com>
2023-04-17 20:37:12 -0500

    Add message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
-
+import { MessageService } from './message.service';
 // register providers (to create service) with injector 
 @Injectable({
   providedIn: 'root' 
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HerroService get heroes');
     return heroes;
   }
 }
